246dd4cf5646aed8eed44f21034963d25642b843
Alladeepthi <169009369+Alladeepthi@users.noreply.github.com>
2024-05-16 19:25:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const backgroundImages = document.querySelectorAll('.background-image');
+
+backgroundImages.forEach(image => {
+  image.style.animationDuration = '30s'; // Adjust animation duration in seconds
+});
